46d9dace0d1087a7dd800a0859b5ddd1086f5959
Lamer0 <combinerk@gmail.com>
2022-10-15 19:03:50 +0300

    Add domain

---
+++ UMan.API/Startup.cs
@@ -0,0 +1,57 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+using UMan.Core.Repositories;
+using UMan.DataAccess;
+using UMan.DataAccess.Repositories;
+using UMan.Domain.Papers;
+
+namespace UMan.API
+{
+    public class Startup
+    {
+        private readonly IConfiguration _config;
+
+        public Startup(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
+            services.AddDbContext<PapersDbContext>(o =>
+            {
+                o.UseSqlServer(_config.GetConnectionString("DbConnection"));
+            });
+
+            services.AddScoped<IRepository<Core.Paper>, PapersRepository>();
+
+            services.AddRouting();
+
+            services.AddMvc();
+        }
+
+        
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
---
+++ UMan.Domain/Papers/Delete.cs
@@ -0,0 +1,31 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UMan.Core;
+using UMan.Core.Repositories;
+
+namespace UMan.Domain.Papers
+{
+    public class Delete
+    {
+        public record Command(int id) : IRequest<bool>;
+
+        public class Handler : IRequestHandler<Command, bool>
+        {
+            private readonly IRepository<Paper> _papersRepository;
+
+            public Handler(IRepository<Paper> papersRepository)
+            {
+                _papersRepository = papersRepository;
+            }
+
+            public async Task<bool> Handle(Command request, CancellationToken cancellationToken)
+            {
+                return await _papersRepository.Delete(request.id, cancellationToken);
+            }
+        }
+    }
+}
